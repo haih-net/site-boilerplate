e23e9d9fdce7d37f04b8d2a2ccc4cdf772a971ac
Nikolay Lanets <n.lanets@freecode.academy>
2025-12-20 12:00:11 +0300

    server: type graphql-shield permissions ruleTree

--- server/graphqlServer/permissions/index.ts
+++ server/graphqlServer/permissions/index.ts
@@ -1,5 +1,40 @@
-import { shield } from 'graphql-shield'
-import { isAuthenticated } from './rules/isAuthenticated'
+import { shield, type IRules } from 'graphql-shield'
+// import { isAuthenticated } from './rules/isAuthenticated'
+
+import type { Mutation, Query, User } from 'src/gql/generated/types'
+
+type UnwrapType<T> = T extends (infer U)[]
+  ? UnwrapType<U>
+  : T extends null | undefined
+    ? never
+    : T
+
+type IsGqlObjectType<T> =
+  UnwrapType<T> extends { __typename?: unknown } ? true : false
+
+type RuleFields<T> = Partial<
+  Record<keyof Omit<T, '__typename'> & string, IRules>
+>
+
+type RuleTreeKeys<T> = keyof Omit<T, '__typename'> & string
+
+type RuleTreeRule<T> =
+  IsGqlObjectType<T> extends true ? IRules | RuleFields<UnwrapType<T>> : IRules
+
+type RuleTree<T> = Partial<
+  Record<RuleTreeKeys<T>, RuleTreeRule<T[RuleTreeKeys<T>]>>
+>
+
+type ExtraPermissionTypes = {
+  User: User
+}
+
+type PermissionsRuleTree = {
+  Query: RuleTree<Query>
+  Mutation: RuleTree<Mutation>
+} & {
+  [K in keyof ExtraPermissionTypes]?: RuleTree<ExtraPermissionTypes[K]> | IRules
+}
 
 const ruleTree = {
   Query: {},
@@ -7,10 +42,7 @@ const ruleTree = {
     // Example: require authentication for specific mutations
     // someProtectedMutation: isAuthenticated,
   },
-}
-
-// Export isAuthenticated for use in other places
-export { isAuthenticated }
+} satisfies PermissionsRuleTree
 
 export const permissions = shield(ruleTree, {
   /**
